efe6e524657fc58ddd25289c16d7b5c846253bd9
Tom O'Connell <toconnel@caltech.edu>
2017-01-16 14:31:39 -0800

    Added preprocessors conditionals to remove / add serial output easily.

--- OlfStimDelivery.ino
+++ OlfStimDelivery.ino
@@ -1,6 +1,8 @@
 #include <RBD_Timer.h>
 
-
+// if you comment out this define, the compiled code will exclude any serial prints
+// to make things slightly more real time
+#define SERIAL_OUTPUT
 
 //set stimulus variables
 const int trialNum = 3;      //number of trials
@@ -11,7 +13,6 @@ const int odorPulseOnset = 10; //onset time of odor pulse in seconds
 
 
 //pin assigments
-
 int scopeDispPin = 3; 
 int olfactometerPin = 5; //6 is 2-butanone, 5 is pentyl acetate
 int scopePin=13;
@@ -28,13 +29,14 @@ RBD::Timer OlfLenTimer;
 
 unsigned long t;
 
-
 // the setup function runs once when you press reset or power the board
 void setup() {
-  //initialize serial
+  #ifdef SERIAL_OUTPUT
   Serial.begin(9600);
+  #endif
 
   // initialize digital pin as an output.
+  // TODO remove or actually use
   pinMode(LED_BUILTIN, OUTPUT);   // built in LED, pulse when odor pulse is being delivered
   pinMode(scopeDispPin, OUTPUT);
   pinMode(scopePin, OUTPUT);
@@ -43,17 +45,18 @@ void setup() {
   digitalWrite(scopeDispPin, LOW);
   digitalWrite(scopePin, LOW);
   digitalWrite(olfactometerPin, LOW);
+
+  #ifdef SERIAL_OUTPUT
   //checks serial
   Serial.println("Serial working");
+  #endif
 
   trialTimer.setTimeout((unsigned long) scopeLen * 1000); //timer runs from start to end of a trial
   OlfStartTimer.setTimeout((unsigned long) odorPulseOnset * 1000);
   OlfLenTimer.setTimeout((unsigned long) odorPulseLen * 1000);
-  
 
+  // TODO so she did try this?
   //trialTimer.restart();
-
-
 }
 
 
@@ -67,7 +70,10 @@ void loop() {
 
     delay(2000);
 
+    #ifdef SERIAL_OUTPUT
     Serial.println("------------Session start------------");
+    #endif
+    
     trialTimer.restart();
     started=started+1;
   }
@@ -75,10 +81,13 @@ void loop() {
   if (trialTimer.onActive()) {
     // TODO remove. not used until redefind.
     t = trialTimer.getValue();
+
     trialInd = trialInd + 1;
     OlfStartTimer.restart();
     digitalWrite(scopeDispPin, HIGH);
     digitalWrite(scopePin, HIGH);
+
+    #ifdef SERIAL_OUTPUT
     Serial.print("Trial # ");
     Serial.print(trialInd);
     Serial.print("/");
@@ -86,6 +95,7 @@ void loop() {
     Serial.print("................");
     t = trialTimer.getValue();
     printTime(t);
+    #endif
   } // end onActive check
 
   if (trialTimer.isActive()) {
@@ -95,30 +105,41 @@ void loop() {
     }
     if (OlfLenTimer.onActive()) {
       digitalWrite(olfactometerPin, HIGH);
+      
+      #ifdef SERIAL_OUTPUT
       Serial.print("\todor pulse start...");
       t = trialTimer.getValue();
       printTime(t);
-
+      #endif
     }
     if (OlfLenTimer.onExpired() && flag == 1) {
       digitalWrite(olfactometerPin, LOW);
+
+      #ifdef SERIAL_OUTPUT
       Serial.print("\todor pulse stop....");
       t = trialTimer.getValue();
       printTime(t);
-
+      #endif
     }
   } // end isActive check
 
   if (trialTimer.onExpired()) {
     digitalWrite(scopeDispPin, LOW);
     digitalWrite(scopePin, LOW);
+
+    #ifdef SERIAL_OUTPUT
     t = trialTimer.getValue();
     Serial.println("Trial complete...........");
     printTime(t);
+    #endif
 
     if (trialInd == trialNum) {
       trialTimer.stop();
+
+      #ifdef SERIAL_OUTPUT
       Serial.println("------------Session complete------------");
+      #endif
+      
       digitalWrite(olfactometerPin, LOW);
       digitalWrite(scopeDispPin, LOW);
       digitalWrite(scopePin, LOW);
@@ -131,10 +152,14 @@ void loop() {
   
 } // end loop
 
+#ifdef SERIAL_OUTPUT
 void printTime(unsigned long t) {
-  Serial.print("t = "); Serial.print(t); Serial.print(" ms");
+  Serial.print("t = ");
+  Serial.print(t);
+  Serial.print(" ms");
   Serial.print("\n");
 }
+#endif
 
 
 
