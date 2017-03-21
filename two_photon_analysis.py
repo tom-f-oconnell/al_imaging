6f44eb38a731ee2c98f1ce8c26f4e914bda05421
Tom O'Connell <toconnel@caltech.edu>
2017-03-21 15:10:00 -0700

    Rewriting two_photon_analysis and analysis. Still haven't tested much of code in match_image_and_sync

--- two_photon_analysis.py
+++ two_photon_analysis.py
@@ -1,12 +1,15 @@
 # Tom O'Connell
 # during rotation in Betty Hong's lab at Caltech, in early 2017
 
+'''
 import importlib
 import tom.analysis
 importlib.reload(tom.analysis)
+'''
 
 # TODO this syntax is frowned upon. just alias analysis.
-from tom.analysis import *
+#from tom.analysis import *
+import tom.analysis as ta
 import argparse
 import glob
 
@@ -18,7 +21,7 @@ plt.close('all')
 # TODO make this repository
 parser = argparse.ArgumentParser(description='Analysis pipeline for 2p movies of ' +\
         'GCaMP signals in the antennal lobe after odor presentation.',
-        epilog='See github.com/tom-f-oconnell/al-imaging for more information.')
+        epilog='See github.com/tom-f-oconnell/al_imaging for more information.')
 
 show_parser = parser.add_mutually_exclusive_group(required=False)
 show_parser.add_argument('-s', '--showplots', dest='show_plots', action='store_true', \
@@ -44,10 +47,11 @@ args = parser.parse_args()
 # initialize seaborn, to make its tweaks to matplotlib (should make things look nicer)
 # TODO can't figure out how to actually change default linewidth
 #sns.set_style('darkgrid')
-# TODO test. does this mean green is plotted first? (it would seem to)
 #sns.set_palette('GnBu_d')
 
 #prefix = '/media/threeA/hong/flies/tifs/xy_motion_corrected/'
+# TODO just use current directory, or make this an option
+# make rest of script agnostic to which directory it is run in (probably already is)
 prefix = '/media/threeA/hong/flies/tifs/thunder_registered/'
 
 # TODO need to expand to include _o1/2/3/anat suffixes
@@ -69,69 +73,88 @@ else:
               ('170214_01e',
                '170215_01e',
                '170215_02e')}
-#)
 
-    # TODO just put these in a consistent position relative to other files ->
-    # maybe assert date / time is close enough to when other files were generated
-    pin2odor_names = {'Mock':
-                       ('2017-02-12_163940.p',
-                        '2017-02-13_145233.p',
-                        '2017-02-13_174625.p'),
-                      '2-butanone 1e-4':
-                       ('2017-02-14_145733.p',
-                        '2017-02-15_095301.p',
-                        '2017-02-15_135620.p')}
+#ta.group_directories(working_directory, ['c', 'e'])
 
-# TODO wasn't there a second fly on the 15th?
-# do any of the earlier flies also have anatomical stacks that are just improperly labeled?
-# look at listing of image objects dimensions?
+"""
+directories -> skip malformed data -> registered stacks in same dirs
+-necessary to automatically screen for trials with drift / loss of signal? (do ~here if so)
 
-# TODO TODO TODO save computed df and images necessary for figures (or figs separately maybe, and maybe not images?)
+registered stacks -> smooth+dF/F
+                     need info about stimulus, possibly including trigger, to group stack into trials
+
+                    -> average across trials within fly -> reject under threshold
+                                                                        for broad odor activation
+                                    -> images via some projection
+                                       (as directory -> (odor -> image))
+                                       -will then have to have a plotting function that deals 
+                                        with this, which would probably be less general
+                                       -or another function that processes this dict
+                                        into one by (fly_id -> (glom -> (odor -> image)))
+
+                                       -> ROI (as directory -> (roi_name -> OpenCV roi))
+                                          (not all images used to make ROIs)
+                                       -> burn in scale bar from metadata (optionally)
+
+                        "" +ROIs -> reject bad ROIs (make GUI to speed this up? / prompt user)
+                                    + save which ROIs were rejected, recompute if code changes
+
+                                    -> individual traces (in data frame) binned in ROIs
+                                         in: ROIs, stacks from same directory (maybe ranges
+                                             of frames from within session at a time?), stimuli
+                                             corresponding to the whole session or ranges of 
+                                             frames currently being processed
+                                         out:
+                                           (multiindexed dataframe, w/ first index as fly_id 
+                                           (derived from directory), and sessions (actual 
+                                            directories) as further index)
+                                       -maybe add fly_id and session # upon return? or pass in?
+                                        in general sessions should be able to have multiple ROIs,
+                                        though in my latest experiments, i am aiming for one glom
+                                        per session
+
+dataframe -> group by rearing condition, glomerulus, and stimuli (???)
+
+plots i want:
+    -fly summaries:
+        -grid for each glomerulus, w/ max projection of avg for each odor
+        -ROIs for each glomerulus (private odors now only exist in sessions w/ suffix of that
+         glomerulus)
+        -individual traces
+        -mean + confidence intervals
+    -averages across flies for each, separate for mock and 2-butanone reared 
+     (glomeruli, stimulus) mean + confidence intervals
+        -one grid per each glomerulus (row w/ private, another row w/ private + PA)
+        -(?) fit slope / sigmoid & summarize w/ 1 number per glomerulus, for private and
+         private + PA
+            -include mock on private series, and include PA on priv + PA row
+"""
+
+"""
+plotting functions i want:
+    -make a grid of images for each terminal dictionary, and title the grids with
+     keys of any containing dictionaries
+        -can use for ROIs and projections
+    -wrapper around facetgrid stuff?
+       -sem or bootstrapping
+       -individual traces
+"""
+
+# TODO when i actually need p2o / stim order meta data, load that from within each dir
+
+# TODO save computed df and images necessary for figures 
+# (or figs separately maybe, and maybe not images?)
 # and only recompute if code checksum differs from code checksum of last version
 
-files = dict()
-for k in flies:
-    files[k] = fix_names(prefix, flies[k], '')
-
-# TODO want plots by glomerulus i think
-# for all the glomeruli that i can compare across the two conditions
-
-# TODO any relevant metadata to correspond sync and imaging data? maybe in the referenced DB?
-# i couldnt readily see it elsewhere
-
-p2o_prefix = '/media/threeA/hong/pins2odors/'
-
-pin2odors = dict()
-
-for k in flies:
-    # load from pin2odor pickled dicts
-    for f in fix_names(p2o_prefix, pin2odor_names[k], ''):
-        with open(f, 'rb') as fp:
-            if not k in pin2odors:
-                pin2odors[k] = []
-
-            pin2odors[k].append(pickle.load(fp))
-
 #pin2odors = [pickle.load(open(f)) for f in fix_names(p2o_prefix, pin2odor_names, '')]
-odor_panel = set()
-for group in pin2odors.values():
-    for fly in group:
-        for experiment in fly:
-            # odor_connections are saved as triples of (arduino pin / valve, odor, manifold port)
-            for odor_connection in experiment:
-                odor_panel.add(odor_connection[1])
-
-# TODO get corresponding data
-#'2017-02-15_135620.p')
 
 suffix = '.tif'
-
-# TODO i feel kinda like i should have all loading of files in this script and 
-# only deal with raw data in tom/analysis
 
 secs_before = 3
 secs_after = 12
 trial_duration = secs_before + secs_after
+
+glomeruli = ('dl5', 'dm6', 'vm7')
 
 # mock / butanone reared
 for condition in sorted(files.keys()):
@@ -142,6 +165,7 @@ for condition in sorted(files.keys()):
         syncdata_files = []
         imaging_files = []
 
+        '''
         for o, p2o in zip(('_o1', '_o2', '_o3'), all_p2o):
 
             if not os.path.exists(full + o + suffix):
@@ -158,13 +182,15 @@ for condition in sorted(files.keys()):
                 p2o_dict = dict(zip(range(5,8), map(lambda x: x[1], p2o)))
             else:
                 p2o_dict = dict(map(lambda x: (x[0], x[1]), p2o))
+        '''
+        for g in glomeruli:
 
             thorsync_file = glob.glob('/media/threeA/hong/flies/' \
                                 + nick + o + '/SyncData*')[0] + '/Episode001.h5'
-
             imaging_file = full + o + suffix
 
             if args.print_summary_only:
+                # TODO what was trial_duration used for?
                 print_odor_order(thorsync_file, p2o_dict, imaging_file, trial_duration)
 
             p2o_dicts.append(p2o_dict)
@@ -182,8 +208,6 @@ for condition in sorted(files.keys()):
             '''
 
             # TODO merge fly_dfs across flies. could probably also just use append.
-
-            #fly_df.isnull().values.ravel().sum()
 
             if fly_df is None:
                 continue
@@ -244,17 +268,6 @@ for condition in sorted(files.keys()):
                 title = 'Mean w/ SEM for ' + glom + ' from blocks ' + str(containing_blocks.unique())
                 g.fig.suptitle(title)
                 g.fig.subplots_adjust(top=0.9)
-
-
-# TODO for each odor known to be a private odor (do i have all the glomeruli i'm interested in
-# covered here?), see which region lights up (dis/con-junction?)
-# (can restrict to only odors coverd in intersection of values sets of pin2odor mappings)
-# and use those regions as definitions of glomeruli to analyze responses to public odors
-
-# TODO plot sums in this regions as traces over time
-
-# TODO compare those regions to where you'd expect them to be from anatomical information alone
-# or do at least their relative positions make sense?
 
 if args.show_plots:
     plt.show()
