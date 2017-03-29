dbda2d4aab6cda1a928f586f9b0fb559a82078ec
Tom O'Connell <toconnel@caltech.edu>
2017-03-28 18:50:05 -0700

    Working on plotting

--- tom/plotting.py
+++ tom/plotting.py
@@ -25,99 +25,8 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 
-import ipdb
 
-# bokeh too later maybe
-
-# TODO how to handle equivalent of subtitles grouping as before?
-# should I?
-# TODO handle at least images and dot / line plotting
-# TODO default to turn off all tickmarks and stuff for images
-
-def reduce(v, c, data):
-    """
-    data: np.ndarray or tuple / list of them
-    v: function that takes elements of data to some scalar (or something comparable)
-    c: function that compares two outputs of v(dn), and returns True or False
-       (condition under which to update state)
-    """
-
-    if type(data) is dict:
-        mapped = tuple(map(lambda x: reduce(v, c, x), data.values()))
-        return reduce(lambda x: x, c, mapped)
-
-    elif type(data) is list or type(data) is tuple:
-        m = data[0]
-
-        for d in data:
-            val = v(d)
-
-            if c(m,val):
-                m = val
-
-        return m
-
-    #elif type(data) == np.ndarray or type(data) == np.float64:
-    else:   
-        return v(data)
-
-    '''
-    else:
-        assert False, 'can only reduce over numpy array or list/tuple of them, not ' + \
-                str(type(data))
-    '''
-
-
-def max_value(data):
-    # this is highlighted, but it is not actually a keyword in Python 3
-    return reduce(np.max, lambda curr, v: curr < v, data)
-
-
-def min_value(data):
-    return reduce(np.min, lambda curr, v: curr > v, data)
-
-
-def first_array(data):
-    if type(data) is dict:
-        v = list(data.values())[0]
-    else:
-        v = data
-
-    # asserts that the groupings are only singly nested, which seems reasonable for now
-    if type(v) is list or type(v) is tuple:
-        return v[0]
-    
-    else:
-        return v
-
-
-def is_image(v):
-    """ assumes things not 1 dimensional are images """
-
-    # only true when one dimension has greater than 1 indices, and all others have 1
-    return not np.prod(v.shape) == np.max(v.shape)
-
-
-def dict2subplots(data_dict, xs, sharex, sharey, avg, cmap, image, emin=None, emax=None):
-    '''
-    data_dict should be of the form:
-        {key (relevant to experimental condition)-> Tx1 (or 1xT? / (T,)?) numpy (?) 1d series
-                                                                       or
-                                                    NxN image
-                                                                       or
-                                                    tuple or list containing exactly one of the above
-        , key2 -> d2, ...}
-
-    other parameters can override some matplotlib defaults
-
-    avg only meaningful if values are tuples or lists, in which case an average for each group
-    of values will also be displayed
-
-    inherits the default kwargs of plot below
-    '''
-
-    # right now we have to pick which images we want from an image series in advance
-
+def plot_image_dict(image_dict):
     key2sbplt = dict()
     sbplt2key = dict()
 
@@ -136,70 +45,26 @@ def dict2subplots(data_dict, xs, sharex, sharey, avg, cmap, image, emin=None, em
     vmax = max_value(data_dict) * 0.3
     vmin = -vmax
     '''
-
     vmax = max_value(data_dict)
     vmin = min_value(data_dict)
-
-    if not image:
-        border = 0.10
-        vmin = vmin * (1 - border)
-        vmax = vmax * (1 + border)
-
-    # TODO integrate into below
-    '''
-                curr = i*cols + j
-
-                if curr >= len(subtitles):
-                    break
-
-                if cols == 1:
-                    ax = axarr[i]
-                else:
-                    ax = axarr[i,j]
-
-                for w in windows[curr]:
-                    ax.plot(times, signal[curr][w[0]:w[1]], alpha=0.6, linewidth=0.3)
-
-                ax.set_ylim((ymin, ymax))
-                ax.fill_between(times, ymin, ymax, where=trigger_in_window.flatten() > 2.5, \
-                        facecolor='black', alpha=0.1)
-
-                curr_pins = set(pin2avg[curr].keys())
-                assert len(curr_pins) == 1, 'havent implemented multiple pins + arbitrary group'
-
-                ax.plot(times, pin2avg[curr][curr_pins.pop()], '-', c='black', alpha=0.6, \
-                        linewidth=1.5, label='Mean')
-
-                if curr == 0:
-                    ax.legend()
-
-                ax.title.set_text(subtitles[curr])
-    '''
 
     # turn off any subplots we wont actually use
     for i in range(rows):
         for j in range(cols):
-            #curr = i*cols + j + 1
             curr = i*cols + j
 
-            # TODO make shorter?
             if cols > 1:
                 ax = axarr[int(np.floor(curr / cols))][curr % cols]
             elif type(axarr) == np.ndarray:
                 ax = axarr[curr]
             else:
                 ax = axarr
-
             sbplt = curr + 1
+
             if sbplt in sbplt2key:
                 k = sbplt2key[sbplt]
 
                 if image:
-                    # TODO:
-                    # -artifacts? only reason scale is off?
-                    # -make plots close together
-                    # -border vs change background color?
-
                     try:
                         img = ax.imshow(data_dict[k], vmin=vmin, vmax=vmax)
                     except TypeError:
@@ -216,16 +81,8 @@ def dict2subplots(data_dict, xs, sharex, sharey, avg, cmap, image, emin=None, em
                     # how is this different from 'box' and 'datalim'? not clipping is it?
                     ax.set_adjustable('box-forced')
 
-                elif not xs is None:
-                    ax.plot(xs, data_dict[k], alpha=0.6, linewidth=0.3)
-
-                else:
-                    # hist?
-                    #assert False, 'not yet implemented'
-                    # TODO fix
-                    clow = emin[k]
-                    chigh = emax[k]
-                    ax.errorbar(np.arange(data_dict[k].shape[0]), data_dict[k], yerr=[clow, chigh])
+            else:
+                raise NotImplementedError
 
                 ax.title.set_text(k)
 
@@ -244,52 +101,21 @@ def dict2subplots(data_dict, xs, sharex, sharey, avg, cmap, image, emin=None, em
 
     # TODO why was only wspace working when both were 0? it might still be only one working
     fig.subplots_adjust(wspace=0.005, hspace=0.2)
-
     return fig
 
 
-def plot(data, xs=None, title=None, sharex=True, sharey=True, avg=True, cmap=None, \
-        window_title=None, emin=None, emax=None, save=False):
-
-    # check whether we are dealing with images or a 1d series
-    image = is_image(first_array(data))
-
-    if image:
-        sns.set_style('dark')
-    else:
-        # is this the right name?
-        sns.set_style('darkgrid')
+def plot(data, title=None, cmap=None, window_title=None, save=False):
+    sns.set_style('dark')
 
     if type(data) is dict:
-        # TODO why did i ever include the xs argument?
-        fig = dict2subplots(data, xs, sharex, sharey, avg, cmap, image, emin, emax)
-        # TODO
+        fig = plot_image_dict(data)
     else:
         fig = plt.figure()
-        #plt.plot / plt.imshow
-
-        if image:
-            plt.imshow(data)
-
-        elif not xs is None:
-            # default dt?
-            plt.plot(xs, data, alpha=0.6, linewidth=0.3)
-
-        else:
-            assert False, 'not yet implemented'
-
-        # TODO if xs is None, histogram?
+        plt.imshow(data)
 
     # will these still work if it plt isn't a subplots object?
     if not title is None:
         plt.suptitle(title)
-
-    if not image:
-    # TODO make these defaults? if i plan to use this besides for PID / images
-        # (I would rather not have to manually set the position of these, but seems like common way
-        #  to do it)
-        fig.text(0.5, 0.04, 'Time (frames)', ha='center')
-        fig.text(0.04, 0.5, r'$\frac{\Delta{}F}{F}$ in ROI', va='center', rotation='vertical')
 
     if not window_title is None:
         fig.canvas.set_window_title(window_title)
@@ -307,7 +133,6 @@ def plot(data, xs=None, title=None, sharex=True, sharey=True, avg=True, cmap=Non
         fig.savefig(fname)
 
         print('done')
-
     return fig
 
 
@@ -404,6 +229,15 @@ def summarize_flies(projections, rois, df, save_to=None):
     # we don't care which condition they came from
     # i feel like i should be able to say 'fly_id' instead of level=1, but i tried...
     grouped = df.groupby(level=1).apply(summarize_fly)
+
+    # TODO
+    for session, image_dict in projections.items():
+        plot(image_dict, title=, window_title, save=True)
+
+    for session, image_dict in rois.items():
+        plot(image_dict, title=, window_title, save=True)
+    
+
 
 
 def summarize_experiment(df, save_to=None):
